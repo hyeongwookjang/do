9033cb24cfc2989c8d70c02ef0eb8bf0c52a0b48
janghw33@gmail.com <janghw33@gmail.com>
2021-12-10 10:27:25 +0900

    Initial commit

---
+++ src/main/java/com/javama/be/TestEx/TestEx.java
@@ -0,0 +1,19 @@
+package com.javama.be.TestEx;
+
+public class TestEx {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        int h = 30;
+
+        char c = 'A';
+        char d = 'a';
+        char e = 'B';
+
+
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
+// 기본 자료형 int double boolean char 등등...
+// 객체 자료형 String system ArrayList 등등...
